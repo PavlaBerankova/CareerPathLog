21680fd0c14cad0b5e3b11d4c20cd0b0dbe32756
Pavla_Beránková <berankovap@hotmail.com>
2023-11-01 14:08:05 +0100

    Test3 ._

---
+++ CareerPathLog/Scene/Testing.swift
@@ -0,0 +1,18 @@
+//
+//  Testing.swift
+//  CareerPathLog
+//
+//  Created by Pavla Beránková on 01.11.2023.
+//
+
+import SwiftUI
+
+struct Testing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Testing()
+}
