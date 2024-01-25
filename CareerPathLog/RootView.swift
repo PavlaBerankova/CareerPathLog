194f10c1678d959759b5a691f673a08859ac6328
Pavla Beránková <berankovap@protonmail.com>
2024-01-25 14:37:17 +0100

    [FEA] Add TabView with badges

--- CareerPathLog/RootView.swift
+++ CareerPathLog/RootView.swift
@@ -1,9 +1,60 @@
 import SwiftUI
 
 struct RootView: View {
+    init() {
+        UITabBarItem.appearance().badgeColor = .black
+    }
+
     var body: some View {
-        OfferListView()
-            .preferredColorScheme(.light)
+        NavigationStack {
+            TabView {
+                OfferListView()
+                    .tabItem {
+                        VStack {
+                            Image.status.allSendCv
+                            Text("Vše")
+                        }
+                    }
+
+                    .badge(10)
+
+                OfferListView()
+                    .tabItem {
+                        VStack {
+                            Image.status.noResponse
+                            Text("Bez odpovědi")
+                        }
+                    }
+                    .badge(10)
+
+                OfferListView()
+                    .tabItem {
+                        VStack {
+                            Image.status.interview
+                            Text("Pohovory")
+                        }
+                    }
+                    .badge(10)
+
+                OfferListView()
+                    .tabItem {
+                        VStack {
+                            Image.status.accepted
+                            Text("Nabídky")
+                        }
+                    }
+                    .badge(5)
+
+                OfferListView()
+                    .tabItem {
+                        VStack {
+                            Image.status.rejected
+                            Text("Zamítnuto")
+                        }
+                    }
+                    .badge(10)
+            }
+        }
     }
 }
 
